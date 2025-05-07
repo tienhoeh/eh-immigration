7115a200d3bc3f42cd9c6c0595930d20fe342875
tienhoeh <tien.ho@eh-immigration.com>
2025-05-07 12:14:57 +0800

    Create auth0_config.js

---
+++ js/auth0_config.js
@@ -0,0 +1,11 @@
+// js/auth0_config.js
+
+// **Important:** Replace with your actual Auth0 Application details
+const auth0Config = {
+    domain: 'eh-immigration.jp.auth0.com',
+    clientId: 'GyTGD7qnwsNkXiXmxVTdVLfJMUlwdAND'
+    // Note: We will specify redirect_uri dynamically in the scripts
+};
+
+// Export the configuration object
+export { auth0Config };
